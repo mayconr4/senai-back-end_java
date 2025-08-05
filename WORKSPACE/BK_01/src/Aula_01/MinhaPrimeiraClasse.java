171691b3966806ae7cfdf8f9813c9d4835a978ca
mayconr4 <mayconhs2007@gmail.com>
2025-08-05 16:12:52 -0300

    Add files via upload

---
+++ WORKSPACE/BK_01/src/Aula_01/MinhaPrimeiraClasse.java
@@ -0,0 +1,18 @@
+package Aula_01;
+
+import javax.swing.JOptionPane;
+
+public class MinhaPrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+		System.out.println("Hello world!");
+		System.out.println("Olá !"); 
+		JOptionPane.showMessageDialog(null, "OLá mundo na tela!!");
+		
+		
+		
+	}
+
+	
+}
---
+++ WORKSPACE/BK_02/src/Aula_01/MinhaSegundaClasse.java
@@ -0,0 +1,22 @@
+package Aula_01;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class MinhaSegundaClasse {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Telefone: 11975040149");
+		System.out.println("Nome: Maycon Henrique");
+		System.out.println("Endereco: Rua Guilhreme de Oliveira");
+
+		JOptionPane.showMessageDialog(null, "Endereco: Rua Guilhreme de Oliveira\nNome: Maycon Henrique\nTelefone: 11975040149 ");
+		
+		
+	}
+
+}
