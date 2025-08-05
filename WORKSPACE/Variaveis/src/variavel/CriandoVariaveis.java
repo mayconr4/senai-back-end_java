bfc9ea710b905b7eb396dfd024706c6cd2990615
mayconr4 <mayconhs2007@gmail.com>
2025-08-05 16:32:41 -0300

    Add files via upload

---
+++ WORKSPACE/Variaveis/src/variavel/CriandoVariaveis.java
@@ -0,0 +1,15 @@
+package variavel;
+
+public class CriandoVariaveis {
+
+	public static void main(String[] args) {
+		byte pequeno = 127;
+		short maior = 32767;
+		int numero = 2_147_483_647;
+		float fracionado = 1.7f;
+		double monetario = 8_147_483_649.00;
+		long infinity =  9_223_372_036_854_775_807l;
+		char letra = 'a';
+	}
+
+}
