52ccbbc8132457ac174acd33d9c0151db7b98ea1
Maycon henrique <mayconhs2007@gmail.com>
2025-08-19 16:54:48 -0300

    Add files via upload

--- WORKSPACE/Exercicios/src/lista_01/Algoritimo_122.java
+++ WORKSPACE/Exercicios/src/lista_01/Algoritimo_122.java
@@ -6,7 +6,7 @@ public class Algoritimo_122 {
 
 	public static void main(String[] args) {
 		
-        int num_01,num_02,num_03;
+        int num_01,num_02,num_03,verifica_01,verifica_02,verifica_03;
 		
 		Scanner ler = new Scanner(System.in);
 		
@@ -18,6 +18,23 @@ public class Algoritimo_122 {
 		
 		System.out.println("Digite um numero");
 		num_03 = ler.nextInt();
+		
+		int[]lados = {num_01,num_02,num_03};
+		
+		for(int lado:lados) {
+		
+		verifica_01 =  lados[0]+ lados[1];
+		verifica_02 =  lados[0]+ lados[2];
+		verifica_03 =  lados[1]+ lados[2];
+		
+		if ( verifica_01 > num_03 && verifica_02 > num_02 && verifica_03 > num_03) {
+			System.out.println("podem ser um lado de um triangulo ");
+		}else {
+			System.out.println("Não podem ser um lado de um triangulo ");
+		}
+		break;	
+		
+		}
 
 	}
 
