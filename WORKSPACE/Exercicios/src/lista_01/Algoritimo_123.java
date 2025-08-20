9062dbbe65eb62fc0b97ce5b8c0aa8f4a20cae95
Maycon henrique <mayconhs2007@gmail.com>
2025-08-20 14:51:10 -0300

    Add files via upload

---
+++ WORKSPACE/Exercicios/src/lista_01/Algoritimo_123.java
@@ -0,0 +1,82 @@
+package lista_01;
+
+import java.util.Scanner;
+
+public class Algoritimo_123 {
+
+	public static void main(String[] args) {
+		
+		int num_01,num_02,num_03,verifica_01,verifica_02,verifica_03;
+		String classifica_triangulo;
+		boolean exemplo = true;
+		
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Digite um numero");
+		num_01 = ler.nextInt();
+		
+		System.out.println("Digite um numero");
+		num_02 = ler.nextInt();
+		
+		System.out.println("Digite um numero");
+		num_03 = ler.nextInt();
+		
+		int[]lados = {num_01,num_02,num_03};
+		
+		for(int lado:lados) {
+			
+			verifica_01 =  lados[0]+ lados[1];
+			verifica_02 =  lados[0]+ lados[2];
+			verifica_03 =  lados[1]+ lados[2];
+			
+			if ( verifica_01 > num_03 && verifica_02 > num_02 && verifica_03 > num_03) {
+				System.out.println("podem ser um lado de um triangulo ");        		
+				
+				Escaleno(lados[0],lados[1],lados[2]);
+				Isoceles(lados[0],lados[1],lados[2]);
+				Equilatero(lados[0],lados[1],lados[2]);
+				
+			}else {
+				System.out.println("Não podem ser um lado de um triangulo ");
+			}
+			break;	
+			
+			}    	
+
+	}
+	
+	public static  int Escaleno(int a, int b, int c) {       		
+		
+		if(!(a == b) && !(c == b) && !(c == a) ) {
+			
+			System.out.println("è um Triangulo Escaleno: ");     		
+		}
+		
+		return a;
+	} 
+	
+	
+	public static int Isoceles(int a, int b, int c) {
+		if (a == b &&  b == a && !(c == b) && !(c == a)   ){
+			
+			System.out.println("è um Triangulo Isóceles: ");
+		}  	 		
+		return a;
+	}
+	
+	
+	public static int Equilatero(int a, int b, int c) {       		
+		
+		if(a == b && c == b && c == a  ) {
+			
+			System.out.println("è um Triangulo Equilátero: ");     			
+		} 		
+		return a;
+	}
+	
+	
+	
+	
+
+}
