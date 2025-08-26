fbfb92d28847bd64ac691daf22c650ee84d41f5c
Maycon henrique <mayconhs2007@gmail.com>
2025-08-26 16:30:21 -0300

    Add files via upload

---
+++ WORKSPACE/POO/src/Zoo/Animal.java
@@ -0,0 +1,45 @@
+package Zoo;
+
+public class Animal {
+	
+	private String raca;
+	private String cor;
+	private String especie;
+	private String tamanho;
+	private double altura;
+	
+	public String getRaca() {
+		return raca;
+	}
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public String getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+	
+
+}
