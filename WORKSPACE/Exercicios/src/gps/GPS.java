5bf476ba90a3e7038e60b31133e9e3b5347c1f5d
Maycon henrique <mayconhs2007@gmail.com>
2025-08-28 13:52:39 -0300

    Add files via upload

--- WORKSPACE/Exercicios/src/gps/GPS.java
+++ WORKSPACE/Exercicios/src/gps/GPS.java
@@ -1,9 +1,48 @@
 package gps;
 
-public class GPS {
+import java.util.Scanner;
+
+public class GPS {   
 	
 	private String idioma;
 	private String rota;
 	
-
+	Scanner ler = new Scanner(System.in);
+	
+	public String getIdioma() {
+		return idioma;
+	}
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+	public String getRota() {
+		return rota;
+	}
+	public void setRota(String rota) {
+		this.rota = rota;
+	}
+	
+	public GPS() {
+		
+	}  
+	
+	public GPS(String idioma,String rota) {
+		this.idioma = idioma;
+		this.rota = rota; 		
+	} 	
+	
+	public String DefineConfiguracoes(String rota, String idioma) {
+		
+		System.out.println("Escolha o idioma: ");
+	 	setIdioma(ler.nextLine());
+		
+		System.out.println("Digite a rota: ");
+		setRota(ler.nextLine());        	
+		return rota;
+	}        
+	
+	public void mostrar() { 
+		System.out.println("Idioma: "+ this.idioma);
+		System.out.println("Rota: "+ this.rota);
+	}
 }
---
+++ WORKSPACE/Exercicios/src/gps/UsaGPS.java
@@ -0,0 +1,12 @@
+package gps;
+
+public class UsaGPS {
+
+	public static void main(String[] args) {
+		GPS gps = new GPS(); 		
+		gps.DefineConfiguracoes(null, null);
+		gps.mostrar(); 		
+
+	}
+
+}
