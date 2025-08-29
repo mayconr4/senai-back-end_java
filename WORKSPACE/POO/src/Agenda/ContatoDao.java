f89e4e7eb832f2f1a0b514a3f50bc0b009506227
Maycon henrique <mayconhs2007@gmail.com>
2025-08-29 16:56:43 -0300

    Add files via upload

--- WORKSPACE/POO/src/Agenda/ContatoDao.java
+++ WORKSPACE/POO/src/Agenda/ContatoDao.java
@@ -1,11 +1,38 @@
 package Agenda;
 
-public class ContatoDao implements Crud{
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
+public class ContatoDao implements Crud{
+	public Contato contato;
+	public BD bd; 	
+	private PreparedStatement stm;
+	private String sql;
+	
+	public ContatoDao() {
+		bd =  new BD();
+		contato = new Contato();
+	} 	
+	
 	@Override
-	public void salvar() {
-		
-		
+	public void salvar() { 		
+		try {
+			sql = "INSERT INTO contato(nome,endereco,telefone,email) VALUES(?,?,?,?)";
+			stm =bd.connection.prepareStatement(sql); 			
+			stm.setString(1, contato.getNome());
+			stm.setString(2, contato.getEndereco());
+			stm.setString(3, contato.getTelefone());
+			stm.setString(4, contato.getEmail()); 			
+			bd.close();
+			
+			
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "erro ao Inserir dados em contato");//eu que escrevi
+			e.printStackTrace();
+		}        	
 	}
 
 	@Override
@@ -23,7 +50,29 @@ public class ContatoDao implements Crud{
 	@Override
 	public void listar() {
 		
-		
+		try {
+			sql = "SELECT nome,endereco,telefone,email FROM contato";
+			stm =bd.connection.prepareStatement(sql);
+			stm.setString(1, contato.getNome());
+			stm.setString(2, contato.getEndereco());
+			stm.setString(3, contato.getTelefone());
+			stm.setString(4, contato.getEmail()); 			
+			System.out.println("Código  Nome                  Endereço              Telefone        E-mail");
+			System.out.println("__________________________________________________________________________");
+			ResultSet resultSet = stm.executeQuery(); 			
+			while(resultSet.next()) {
+				String nome = resultSet.getString("nome");
+				String endereco = resultSet.getString("endereco");
+				String telefone = resultSet.getString("telefone");
+				String email = resultSet.getString("email");
+				System.out.println(nome+"  "+endereco+"           "+telefone+"         "+email);	
+				stm.close();
+				bd.close();
+			}
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "erro ao listar dados em contato");
+			e.printStackTrace();
+		}
 	}
 
 }
