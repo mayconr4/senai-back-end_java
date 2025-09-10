0b36f57b5bcc7484c59feb64162971afa115dd1d
Maycon henrique <mayconhs2007@gmail.com>
2025-09-10 13:19:39 -0300

    Add files via upload

--- WORKSPACE/suporte/src/main/java/br/com/suporteSenai/suporte/controller/SolicitacaoController.java
+++ WORKSPACE/suporte/src/main/java/br/com/suporteSenai/suporte/controller/SolicitacaoController.java
@@ -1,15 +1,27 @@
 package br.com.suporteSenai.suporte.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.suporteSenai.suporte.model.Solicitacao;
+import br.com.suporteSenai.suporte.repository.SolicitacaoRepository;
 
 @Controller
 public class SolicitacaoController {
 	
-	@GetMapping("/solicitacao")
+	@Autowired
+	private SolicitacaoRepository sr;
+	
+	@GetMapping(value="/solicitacao")
 	public String solicitacao() {
 		
 		return "solicitacao";
 	}
-	
+		@PostMapping(value = "/solicitacao")
+		public String solicitacao(Solicitacao solicitacao) {
+			sr.save(solicitacao);
+			return "redirect:/solicitacao";
+		}
 }
